692fc8ef04226a4070e4c02c3de64a7e628767d9
Robert <rgriffin@ihmc.us>
2018-08-06 11:50:20 -0500

    fixed the frame issues for the quadrat6ic distance and yaw cost

--- ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepCost/QuadraticDistanceAndYawCost.java
+++ ihmc-footstep-planning/src/main/java/us/ihmc/footstepPlanning/graphSearch/stepCost/QuadraticDistanceAndYawCost.java
@@ -1,9 +1,15 @@
 package us.ihmc.footstepPlanning.graphSearch.stepCost;
 
+import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.euclid.referenceFrame.FramePose3D;
+import us.ihmc.euclid.referenceFrame.FrameVector2D;
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
 import us.ihmc.euclid.tuple2D.Point2D;
+import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.footstepPlanning.graphSearch.FootstepPlannerParameters;
 import us.ihmc.footstepPlanning.graphSearch.graph.FootstepNode;
 import us.ihmc.robotics.geometry.AngleTools;
+import us.ihmc.robotics.referenceFrames.PoseReferenceFrame;
 
 public class QuadraticDistanceAndYawCost implements FootstepCost
 {
@@ -13,6 +19,10 @@ public class QuadraticDistanceAndYawCost implements FootstepCost
    private static final double yCost = 2.0;
    private static final double yawCostScaler = 10.0;
 
+   private final FramePoint3D endNodePosition = new FramePoint3D();
+   private final FramePose3D startNodePose = new FramePose3D(ReferenceFrame.getWorldFrame());
+   private final PoseReferenceFrame startNodeFrame = new PoseReferenceFrame("startNodeFrame", ReferenceFrame.getWorldFrame());
+
    public QuadraticDistanceAndYawCost(FootstepPlannerParameters parameters)
    {
       this.parameters = parameters;
@@ -21,12 +31,19 @@ public class QuadraticDistanceAndYawCost implements FootstepCost
    @Override
    public double compute(FootstepNode startNode, FootstepNode endNode)
    {
-      // TODO is this in the correct frame? No, no it's not.
       Point2D startPoint = startNode.getOrComputeMidFootPoint(parameters.getIdealFootstepWidth());
       Point2D endPoint = endNode.getOrComputeMidFootPoint(parameters.getIdealFootstepWidth());
+
+      startNodePose.setPosition(startPoint.getX(), startPoint.getY(), 0.0);
+      startNodePose.setOrientationYawPitchRoll(startNode.getYaw(), 0.0, 0.0);
+      startNodeFrame.setPoseAndUpdate(startNodePose);
+
+      endNodePosition.setIncludingFrame(ReferenceFrame.getWorldFrame(), endPoint, 0.0);
+      endNodePosition.changeFrame(startNodeFrame);
+
       double euclideanDistance = startPoint.distance(endPoint);
-      double cost = xCost * Math.pow(endPoint.getX() - startPoint.getX(), 2.0);
-      cost += yCost * Math.pow(endPoint.getY() - startPoint.getY(), 2.0);
+      double cost = xCost * Math.pow(endNodePosition.getX(), 2.0);
+      cost += yCost * Math.pow(endNodePosition.getY(), 2.0);
 
       double yaw = AngleTools.computeAngleDifferenceMinusPiToPi(startNode.getYaw(), endNode.getYaw());
 
