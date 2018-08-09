012285601074f3f4a7bd3a0dc0afdd4a7224bab7
InhoLee <ilee@ihmc.us>
2018-08-09 15:53:04 -0500

    Refactored name of topic for whole body trajectory toolbox.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/joystickBasedJavaFXController/GraspingToolboxPaneController.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/joystickBasedJavaFXController/GraspingToolboxPaneController.java
@@ -1,18 +1,17 @@
 package us.ihmc.avatar.joystickBasedJavaFXController;
 
-import us.ihmc.commons.PrintTools;
 import us.ihmc.javaFXToolkit.messager.JavaFXMessager;
 
 public class GraspingToolboxPaneController
 {
    public GraspingToolboxPaneController()
    {
-      
+
    }
 
    public void initialize(JavaFXMessager messager)
    {
-      PrintTools.info("initialize");
+      
    }
 
 }
