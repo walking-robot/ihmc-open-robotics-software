ff8bc908f22849401bc66f6c0496b57f6194098e
Stephen McCrory <smccrory@ihmc.us>
2018-08-16 17:24:49 -0500

    initializing swing trajectory when setting footstep instead of waiting for onEntry. adding a call to doOptimizationUpdate at the end of initialization

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/SwingState.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/controlModules/foot/SwingState.java
@@ -384,8 +384,6 @@ public class SwingState extends AbstractFootControlState
       contactState.notifyContactStateHasChanged();
 
       spatialFeedbackControlCommand.resetSecondaryTaskJointWeightScale();
-
-      initializeTrajectory();
    }
 
    @Override
@@ -551,6 +549,7 @@ public class SwingState extends AbstractFootControlState
 
       adjustedFootstepPose.set(footstepPose);
       rateLimitedAdjustedPose.set(footstepPose);
+      initializeTrajectory();
    }
 
    /**
@@ -736,8 +735,8 @@ public class SwingState extends AbstractFootControlState
       swingTrajectoryOptimizer.setSwingHeight(swingHeight.getDoubleValue());
       swingTrajectoryOptimizer.setStanceFootPosition(stanceFootPosition);
       swingTrajectoryOptimizer.initialize();
+      swingTrajectoryOptimizer.doOptimizationUpdate();
    }
-
 
    private void computeCurrentWeights(Vector3DReadOnly nominalAngularWeight, Vector3DReadOnly nominalLinearWeight, Vector3DBasics currentAngularWeightToPack,
                                         Vector3DBasics currentLinearWeightToPack)
